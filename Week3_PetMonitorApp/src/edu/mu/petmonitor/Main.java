32ba5c6365de87d3fa5c6e1128691c81ae3cb2c4
ekincanufuktepe <ekincanufuktepe@gmail.com>
2025-02-07 14:09:46 -0600

    Main class added to run the program

---
+++ Week3_PetMonitorApp/src/edu/mu/petmonitor/Main.java
@@ -0,0 +1,24 @@
+package edu.mu.petmonitor;
+
+import edu.mu.petmonitor.userprofiles.Pet;
+import edu.mu.petmonitor.userprofiles.PetOwner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		PetOwner owner = new PetOwner();
+		System.out.println("Name: " + owner.getFirstname());
+		
+		PetOwner ownerWithName = new PetOwner("Ekincan", "Ufuktepe");
+		System.out.println("Name: " + ownerWithName.getFirstname());
+		
+		ownerWithName.adoptPet(new Pet("Peanut"));
+		
+		System.out.println("Ekin's pets:");
+		for(int i=0; i<ownerWithName.getNumOfPets(); i++) {
+			System.out.println("Pet: " + ownerWithName.getPets()[i]);
+		}
+		
+	}
+
+}
