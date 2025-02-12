454709876136766c040719b779d66e20b04a37de
ekincanufuktepe <ekincanufuktepe@gmail.com>
2025-02-12 13:55:42 -0600

    Adding PetSpecies as a field to Pet.

--- Week3_PetMonitorApp/src/edu/mu/petmonitor/userprofiles/Pet.java
+++ Week3_PetMonitorApp/src/edu/mu/petmonitor/userprofiles/Pet.java
@@ -4,14 +4,33 @@ public class Pet {
 	
 	private String name;
 	private final int numberOfLegs = 8;
+	private PetSpecies species;
 	
+	public PetSpecies getSpecies() {
+		return species;
+	}
+
+	public void setSpecies(PetSpecies species) {
+		this.species = species;
+	}
+
 	@Override
 	public String toString() {
 		return "Pet [name=" + name + ", numberOfLegs=" + numberOfLegs + "]";
 	}
+	
+	/**
+	 * Copy Constructor for the Pet class
+	 * @param pet
+	 */
+	public Pet(Pet pet) {
+		this.name = pet.getName();
+		this.species = pet.getSpecies();
+	}
 
 	public Pet(String name) {
 		this.name = name;
+		this.species = PetSpecies.CAT;
 	}
 	
 	public String getName() {
