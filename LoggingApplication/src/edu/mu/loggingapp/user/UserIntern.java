bdcbb544dab0c1c3951963a75ddb434f43cdd631
ekincanufuktepe <ekincanufuktepe@gmail.com>
2025-02-17 13:52:57 -0600

    UserIntern class added

---
+++ LoggingApplication/src/edu/mu/loggingapp/user/UserIntern.java
@@ -0,0 +1,28 @@
+package edu.mu.loggingapp.user;
+
+import edu.mu.loggingapp.logger.LoggerSingleton;
+
+public class UserIntern {
+	private static int IDCounter = 1;
+	private int internID;
+	
+	public UserIntern() {
+		this.internID = IDCounter;
+		IDCounter++;
+	}
+
+	public int getDeveloperID() {
+		return internID;
+	}
+	
+	public void addAction() {
+		String msg = "Intern #" + internID + " added something.";
+		LoggerSingleton.getInstance().logUser(msg);  
+	}
+	
+	public void deleteAction() {
+		String msg = "Intern #" + internID + " deleted something.";
+		LoggerSingleton.getInstance().logUser(msg);
+	}
+
+}
