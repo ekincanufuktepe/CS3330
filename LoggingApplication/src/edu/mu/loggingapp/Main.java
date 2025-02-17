1f4adfd438092305f4de32fcbcc9bd5443bcb759
ekincanufuktepe <ekincanufuktepe@gmail.com>
2025-02-17 13:54:07 -0600

    Adding the Main class with the main method

---
+++ LoggingApplication/src/edu/mu/loggingapp/Main.java
@@ -0,0 +1,18 @@
+package edu.mu.loggingapp;
+
+import edu.mu.loggingapp.user.UserDeveloper;
+import edu.mu.loggingapp.user.UserIntern;
+
+public class Main {
+
+	public static void main(String[] args) {
+		UserDeveloper dev1 = new UserDeveloper();
+		dev1.addAction();
+		dev1.addAction();
+		UserDeveloper dev2 = new UserDeveloper();
+		dev2.addAction();
+		UserIntern intern1 = new UserIntern();
+		intern1.deleteAction();
+	}
+
+}
