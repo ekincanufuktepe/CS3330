4d1a00ebe3b5fc1669fc21df390163313261ac6b
ekincanufuktepe <ekincanufuktepe@gmail.com>
2025-02-17 13:53:41 -0600

    Adding the class with the Singleton design pattern example.

---
+++ LoggingApplication/src/edu/mu/loggingapp/logger/LoggerSingleton.java
@@ -0,0 +1,39 @@
+package edu.mu.loggingapp.logger;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class LoggerSingleton {
+
+	private static LoggerSingleton instance;
+	private static final String filename = "./files/log.txt";
+	
+	// prevent from being invoked outside
+	private LoggerSingleton() {
+		
+	}
+	
+	public static LoggerSingleton getInstance() {
+		if(instance == null) {
+			instance = new LoggerSingleton();
+		}
+		return instance;
+	}
+	
+	public void logUser(String message) {
+		BufferedWriter bw = null;
+		try {
+			bw = new BufferedWriter(new FileWriter(filename, true));
+			String timestamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+			bw.write("["+ timestamp + "] " + message + "\n");
+			bw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+}
